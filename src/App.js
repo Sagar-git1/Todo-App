fc1ae5b9cb87527f4721f5c881cc285839b56f5f
LakshayBabbar <lakshaybabbar0118@gmail.com>
2023-10-02 11:13:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList/TodoList';
+function App() {
+  const [Todos, setTodo] = useState([]);
+
+  const onFormData = (formData) => {
+    setTodo((prevTodos) => { return [formData, ...prevTodos] });
+    console.log(formData);
+  }
+
+  const deleteItem = (id) => {
+    setTodo((prevTodos)=>{
+      return prevTodos.filter((updateTodo, index)=>{
+        return index !== id;
+      })
+    })
+  }
+
+  return (
+    <div className="wrapper">
+      <TodoForm onAddData={onFormData} />
+      <TodoList elements={Todos} idSelector={deleteItem}/>
+    </div>
+  );
+
+}
+
+export default App;
